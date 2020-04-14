41d687365039f3d776cea7d39b46178435bde0fb
Makalkin <alekseymakalkin00@gmail.com>
2020-04-15 02:20:52 +0300

    Version 2.0.0 (несколько комментариев в коде)

--- app/js/index.js
+++ app/js/index.js
@@ -52,15 +52,16 @@ loadMore.onclick = function() {
 
 
 
-
+// Переменная маленького окна поиска
 var search = document.querySelector(".search");
 
+// При клике на окно поиска выскакивает окно поиска на весь экран
 search.onclick = function() {
     search.style.visibility = "hidden";
     document.querySelector(".searchFullScrin").style.display = "block";
 }
 
-
+// При клике на кнопку "Закрыть" под большим окном поиска, это окно закрывается
 document.querySelector(".search-input-full input").onclick = function() {
     search.style.visibility = "visible";
     document.querySelector(".searchFullScrin").style.display = "none";
